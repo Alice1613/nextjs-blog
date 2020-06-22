84102b5916090806748218706e6a8646258d9842
Alice1613 <66433768+Alice1613@users.noreply.github.com>
2020-06-22 11:20:03 +0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -6,29 +6,29 @@ import Link from 'next/link'
 import Date from '../components/date'
 
 
-export default function Home({ allPostsData }) {
-  return (
-    <Layout home>
-      <Head>…</Head>
-      <section className={utilStyles.headingMd}>…</section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-  <Link href="/posts/[id]" as={`/posts/${id}`}>
-    <a>{title}</a>
-  </Link>
-  <br />
-  <small className={utilStyles.lightText}>
-    <Date dateString={date} />
-  </small>
-</li>
-          ))}
-        </ul>
-      </section>
-    </Layout>
-  )
+  export default function Home({ allPostsData }) {
+    return (
+      <Layout home>
+        <Head>…</Head>
+        <section className={utilStyles.headingMd}>…</section>
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+          <h2 className={utilStyles.headingLg}>Blog</h2>
+          <ul className={utilStyles.list}>
+            {allPostsData.map(({ id, date, title }) => (
+              <li className={utilStyles.listItem} key={id}>
+                  <Link href="/posts/[id]" as={`/posts/${id}`}>
+                    <a>{title}</a>
+                  </Link>
+                  <br />
+                  <small className={utilStyles.lightText}>
+                    <Date dateString={date} />
+                  </small>
+                </li>
+                          ))}
+                        </ul>
+                      </section>
+                    </Layout>
+                  )
 }
 
 export async function getStaticProps() {
